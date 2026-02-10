409d4a66ad9709fae0aa50737a5f548b74078237
jflevig <jfelevi@gmail.com>
2026-02-10 16:15:13 -0300

    Se agrega Variable de Estado para la BD

--- minix_project/settings.py
+++ minix_project/settings.py
@@ -90,7 +90,10 @@ WSGI_APPLICATION = 'minix_project.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600)
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
 
 # Password validation
